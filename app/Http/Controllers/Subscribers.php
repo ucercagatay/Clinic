13ff5e42bfb5c65ab7180ee4a73c26a88a5bf64e
ucercagatay <cagataygia@gmail.com>
2021-09-30 23:18:22 +0300

    Panele eklemeler yapıldı

--- app/Http/Controllers/Subscribers.php
+++ app/Http/Controllers/Subscribers.php
@@ -14,6 +14,7 @@ class Subscribers extends Controller
             'created_at'=>now(),
             'updated_at'=>now(),
         ]);
+        return back();
     }
     public function showSubscriber(){
         $subscribers=Subscriber::all();
