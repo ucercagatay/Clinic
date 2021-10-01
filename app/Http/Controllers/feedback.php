f3ceff27f989b6c608bf1df34ef9c6c4236d0f02
ucercagatay <cagataygia@gmail.com>
2021-10-02 00:47:57 +0300

    son 2 işlem kaldı

--- app/Http/Controllers/feedback.php
+++ app/Http/Controllers/feedback.php
@@ -14,7 +14,6 @@ class feedback extends Controller
 
     public function message(Request $request)
     {
-
         $validated=$request->validate([
             'name' =>'required|min:3|max:30',
             'surname'=>'required|min:3|max:24',
@@ -23,27 +22,18 @@ class feedback extends Controller
             'option'=>'required',
             'message'=>'min:3|max:240',
         ]);
-        if($validated==1){
-            $message=DB::table('contacts')->insert([
-                'name'=>$request->input('name'),
-                'surname'=>$request->input('surname'),
-                'email'=>$request->input('email'),
-                'phoneNumber'=>$request->input('phoneNumber'),
-                'option'=>$request->input('option'),
-                'message'=>$request->input('message'),
-                'created_at'=>now(),
-                'updated_at'=>now(),
-            ]);
-            if($message==1){
-                return 'Mesajınız gönderilmiştir';
-            }
-            else{
-                return 'Mesajınız gönderilemedi';
-            }
-
-        }
-
-        }
+          DB::table('contacts')->insert([
+              'name'=>$request->input('name'),
+              'surname'=>$request->input('surname'),
+              'email'=>$request->input('email'),
+              'phoneNumber'=>$request->input('phoneNumber'),
+              'option'=>$request->input('option'),
+              'message'=>$request->input('message'),
+              'created_at'=>now(),
+              'updated_at'=>now(),
+          ]);
+       return back();
+      }
 
 
 }
