3c0d46e94d4eaa067950f026e731a4e4a62d41ac
Yusuf Çağlar Aksoy <77413549+YusufCaglarAksoy@users.noreply.github.com>
2021-10-03 11:54:45 +0300

    styles has been updated

--- resources/views/gözBurunEstetik.blade.php
+++ resources/views/gözBurunEstetik.blade.php
@@ -16,18 +16,19 @@
 
     <section class="inner-page">
         <div class="container">
-            <img class="sol" src="{{ asset("front/images/inner-page/göz.jpg") }}" alt="">
-            <h2>Göz Estetiği</h2>
-            <p>
-                Erkan Şanlı Hair Clinicte  başta ( Akıllı mercek ameliyatı ), Katarakt, Miyop, Glokom ( Göz Tansiyonu), Oküloplasti ( Göz Estetiği ), Çocuk Göz Hastalıkları, ve astigmat gibi göz hastalıkları, lasik göz ameliyatları ile tedavileri yapılmaktadır</p>
-            <br>
-            <img class="sag" src="{{ asset("front/images/inner-page/burun.png") }}" alt="">
+            <img class="sol" src="{{ asset("front/images/inner-page/burun.png") }}" alt="">
             <h2>Burun Estetiği</h2>
             <p>Burun estetiği, ya da diğer adıyla rinoplasti burun şeklini değiştirmek amacıyla yapılan cerrahi operasyonlara verilen isimdir.
                 Burun estetiği yaptırmanın nedenleri arasında bir yaralanmadaki deformitelerin onarılması, veya doğum kusurunun düzeltilmesi için burnun fiziksel görünümünü değiştirmek ya da bazı solunum güçlüklerinin iyileştirerek, nefes almayı kolaylaştırmak bulunmaktadır.
             </p>
             <p>
                 Bazı vakalarda hem solunum güçlükleri hem de fiziksel görünüm rinoplasti seçeneğinin tercih edilmesinin arkasındaki nedenler arasında yer alabilir.</p>
+            <br><br><br><br><br><br><br>
+            <img class="sag" src="{{ asset("front/images/inner-page/göz.jpg") }}" alt="">
+            <h2>Göz Estetiği</h2>
+            <p>
+                Erkan Şanlı Hair Clinicte  başta ( Akıllı mercek ameliyatı ), Katarakt, Miyop, Glokom ( Göz Tansiyonu), Oküloplasti ( Göz Estetiği ), Çocuk Göz Hastalıkları, ve astigmat gibi göz hastalıkları, lasik göz ameliyatları ile tedavileri yapılmaktadır</p>
+
 
 </div>
     </section>
